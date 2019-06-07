9c9f9daaf6526ecd99ea2e51d58fc9a1ae9a96f7
matthew.barnekow <matthew.barnekow@fuseintegration.com>
2019-06-06 18:13:32 -0700

    Added more examples of how to implement bash options and renamed

--- bash_user_options_ex.sh
+++ bash_user_options_ex.sh
@@ -65,4 +65,67 @@ pakckge=""
 target=""
 #above vars are defined at beginning of script
 
-#
+#Simpler option example using bash loops to loop over $@
+#$@=array of passed args $1 is first one $2 is second etc
+#$#= number of args passed
+#$?= the value returned by last function
+
+#passes all cases off to handler functions
+if [ $# -eq 0 ] ; then
+    handle_help
+    exit 0
+fi
+
+for var in "$@"
+do
+    case $var in
+        --help|-h)
+            handle_help 0
+            exit 0
+            ;;
+
+        --version|-v) #example of long and short switches captured with or
+            handle_version
+            exit 0
+            ;;
+        *) #this is linux all. Catches all other commands passed
+            handle_error
+            exit 1
+            ;;
+    esac
+done
+
+
+#Example with non optional arguments only
+#loops the same for error handling
+for var in "$@"
+do
+    if [ "$var" = "--help" -o "$var" = "-h" ]
+    then
+       handle_help 0
+       exit 0
+    fi;
+
+    if [ "$var" = "--version" -o "$var" = "-v" ]
+    then
+        handle_version
+        exit 0
+    fi;
+done
+#checks if there is an argument errors out and moves on to next
+if [ "$1" ]
+then
+    echo statement #do stuff here if present
+    exit 1
+else
+    echo "No argument given for install_prefix"
+    exit 0
+fi
+
+if [ "$2" ]
+then
+    echo statement #do stuff here if present
+else
+    echo "No argument given for config_file"
+    exit 0
+fi
