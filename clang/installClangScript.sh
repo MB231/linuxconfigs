caec0d603f133f9762336bcaee1cae2870d33cad
Matthew Barnekow <matthew.barnekow@fuseintegration.com>
2019-11-12 11:18:15 -0800

    made clang install based on 9.0 downloaded source code builds or from git build

--- clang/installClangScript.sh
+++ clang/installClangScript.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-tar -xvf llvm-9.0.0.src.tar.xz
-cd llvm-9.0.0.src
+#put loop for all of llvm9 repos, until then place in each directory and run
+#tar -xvf llvm-9.0.0.src.tar.xz
+#cd llvm-9.0.0.src
 mkdir build
 cd build
-cmake3 -DCMAKE_BUILD_TYPE=Release ../ && make -j3
+cmake3 -DCMAKE_BUILD_TYPE=Release ../
+make -j3
 sudo cmake3 --build . --target install
 
